2b78508223a8cf10debf2dd9e6ba501c04470443
Jonas Cleve <github@bcdf.eu>
2014-08-31 23:49:44 +0200

    Remove main code

--- src/imapidle.py
+++ src/imapidle.py
@@ -27,24 +27,3 @@ def done(connection):
 
 imaplib.IMAP4.idle = idle
 imaplib.IMAP4.done = done
-
-if __name__ == '__main__':
-    import os
-    from lamson.mail import MailRequest
-    user = os.environ['EMAIL']
-    password = os.environ['PASSWORD']
-    print os.environ['SERVER']
-    conn = imaplib.IMAP4_SSL(os.environ['SERVER'])
-    conn.login(user, password)
-    conn.select()
-    loop = True
-    while loop:
-        for uid, msg in conn.idle():
-            print uid, msg
-            if msg == "EXISTS":
-                conn.done()
-                status, datas = conn.fetch(uid, '(RFC822)')
-                m = MailRequest('localhost', None, None, datas[0][1])
-                print m.keys()
-                print m.all_parts()
-                print m.is_bounce()
